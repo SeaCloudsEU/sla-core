e6aae9f7f5c988048ff9ea497f04359be5679610
Román SG <roman.sosa@atos.net>
2014-12-02 15:31:12 +0000

    bin/runserver scripts accepts port listening parameter

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -15,4 +15,9 @@
 #    limitations under the License.
 #
 
-mvn tomcat:run -f sla-service/pom.xml
+#
+# Usage: $0 [port]
+#   port: listening port; defaults to 8080
+#
+PORT=${1:-8080}
+mvn tomcat:run -f sla-service/pom.xml -Dmaven.tomcat.port=$PORT
